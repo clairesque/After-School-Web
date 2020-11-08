d30c38d389d0ccf71e5acad715d227ca34a7d6ed
clairesque <clerissalewis@gmail.com>
2020-11-08 10:57:50 +0400

    Displayed all subjects with details

---
+++ subjects.js
@@ -0,0 +1,28 @@
+var subjects = [{
+	"id": 1,
+    "name": "Computer Science",
+    "location": "Dubai",
+	"price": 100,
+	"image": "images/dog.png",
+	"availableInventory": 5,
+	"rating": 1
+}, 
+{
+	"id": 2,
+    "name": "Mathematics",
+    "location": "Abu Dhabi",
+	"price": 150,
+	"image": "images/dog.png",
+	"availableInventory": 5,
+	"rating": 3
+},
+{
+	"id": 3,
+    "name": "Biology",
+    "location": "Dubai",
+	"price": 200,
+	"image": "images/dog.png",
+	"availableInventory": 5,
+	"rating": 5
+}
+]
