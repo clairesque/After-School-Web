7ba77badea4d2fac2f0e883f09dc5f8eba018066
clairesque <clerissalewis@gmail.com>
2020-11-12 10:03:32 +0400

    Designed products and added images

--- subjects.js
+++ subjects.js
@@ -1,9 +1,39 @@
 var subjects = [{
+	"id": 7,
+    "name": "Art",
+    "location": "Dubai",
+	"price": 100,
+	"image": "images/044-watercolor.png",
+	"availableInventory": 5,
+	"spaces":5,
+	"rating": 1
+}, 
+{
+	"id": 8,
+    "name": "Music",
+    "location": "Abu Dhabi",
+	"price": 150,
+	"image": "images/025-tuba.png",
+	"availableInventory": 5,
+	"spaces":5,
+	"rating": 3
+},
+{
+	"id": 9,
+    "name": "Sports",
+    "location": "Dubai",
+	"price": 200,
+	"image": "images/048-backboard.png",
+	"availableInventory": 5,
+	"spaces":5,
+	"rating": 5
+},
+{
 	"id": 1,
     "name": "Computer Science",
     "location": "Dubai",
 	"price": 100,
-	"image": "images/dog.png",
+	"image": "images/020-computer.png",
 	"availableInventory": 5,
 	"spaces":5,
 	"rating": 1
@@ -13,7 +43,7 @@ var subjects = [{
     "name": "Mathematics",
     "location": "Abu Dhabi",
 	"price": 150,
-	"image": "images/dog.png",
+	"image": "images/007-mathematics.png",
 	"availableInventory": 5,
 	"spaces":5,
 	"rating": 3
@@ -23,7 +53,67 @@ var subjects = [{
     "name": "Biology",
     "location": "Dubai",
 	"price": 200,
-	"image": "images/dog.png",
+	"image": "images/029-microscope.png",
+	"availableInventory": 5,
+	"spaces":5,
+	"rating": 5
+},
+{
+	"id": 4,
+    "name": "Geometry",
+    "location": "Dubai",
+	"price": 100,
+	"image": "images/027-compass.png",
+	"availableInventory": 5,
+	"spaces":5,
+	"rating": 1
+}, 
+{
+	"id": 5,
+    "name": "Chemistry",
+    "location": "Abu Dhabi",
+	"price": 150,
+	"image": "images/056-chemical.png",
+	"availableInventory": 5,
+	"spaces":5,
+	"rating": 3
+},
+{
+	"id": 6,
+    "name": "Physics",
+    "location": "Dubai",
+	"price": 200,
+	"image": "images/050-newton-cradle.png",
+	"availableInventory": 5,
+	"spaces":5,
+	"rating": 5
+},
+{
+	"id": 10,
+    "name": "History",
+    "location": "Dubai",
+	"price": 100,
+	"image": "images/033-hourglass.png",
+	"availableInventory": 5,
+	"spaces":5,
+	"rating": 1
+}, 
+{
+	"id": 11,
+    "name": "Geography",
+    "location": "Abu Dhabi",
+	"price": 150,
+	"image": "images/012-globe.png",
+	"availableInventory": 5,
+	"spaces":5,
+	"rating": 3
+},
+{
+	"id": 12,
+    "name": "English",
+    "location": "Dubai",
+	"price": 200,
+	"image": "images/042-note.png",
 	"availableInventory": 5,
 	"spaces":5,
 	"rating": 5
