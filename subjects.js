5f7de9825906f2c3c1b6492245f3bf8124af2e45
clairesque <clerissalewis@gmail.com>
2020-11-14 18:47:00 +0400

    Fixed sorting bug

--- subjects.js
+++ subjects.js
@@ -1,122 +1,110 @@
 var subjects = [{
 	"id": 7,
 	"name": "Art",
-	"location": "Dubai",
+	"location": "Deira",
 	"price": 100,
 	"image": "images/044-watercolor.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 1
+	"spaces": 5
 },
 {
 	"id": 8,
 	"name": "Music",
-	"location": "Abu Dhabi",
-	"price": 150,
+	"location": "International City",
+	"price": 300,
 	"image": "images/025-tuba.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 3
+	"spaces": 5
 },
 {
 	"id": 9,
 	"name": "Sports",
-	"location": "Dubai",
-	"price": 200,
+	"location": "Karama",
+	"price": 250,
 	"image": "images/048-backboard.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 5
+	"spaces": 5
 },
 {
 	"id": 1,
 	"name": "Computer Science",
-	"location": "Dubai",
-	"price": 100,
+	"location": "Deira",
+	"price": 400,
 	"image": "images/020-computer.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 1
+	"spaces": 5
 },
 {
 	"id": 11,
 	"name": "Geography",
-	"location": "Abu Dhabi",
+	"location": "Al Nahda",
 	"price": 150,
 	"image": "images/012-globe.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 3
+	"spaces": 5
 },
 {
 	"id": 2,
 	"name": "Mathematics",
-	"location": "Abu Dhabi",
-	"price": 150,
+	"location": "Al Barsha",
+	"price": 350,
 	"image": "images/007-mathematics.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 3
+	"spaces": 5
 },
 {
 	"id": 3,
 	"name": "Biology",
-	"location": "Dubai",
-	"price": 200,
+	"location": "Business Bay",
+	"price": 100,
 	"image": "images/029-microscope.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 5
+	"spaces": 5
 },
 {
 	"id": 4,
 	"name": "Geometry",
-	"location": "Dubai",
-	"price": 100,
+	"location": "Emirates Hills",
+	"price": 250,
 	"image": "images/027-compass.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 1
+	"spaces": 5
 },
 {
 	"id": 5,
 	"name": "Chemistry",
-	"location": "Abu Dhabi",
-	"price": 150,
+	"location": "Jebel Ali",
+	"price": 300,
 	"image": "images/056-chemical.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 3
+	"spaces": 5
 },
 {
 	"id": 6,
 	"name": "Physics",
-	"location": "Dubai",
-	"price": 200,
+	"location": "Al Nahda",
+	"price": 500,
 	"image": "images/050-newton-cradle.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 5
+	"spaces": 5
 },
 {
 	"id": 10,
 	"name": "History",
-	"location": "Dubai",
-	"price": 100,
+	"location": "International City",
+	"price": 450,
 	"image": "images/033-hourglass.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 1
+	"spaces": 5
 },
 
 {
 	"id": 12,
 	"name": "English",
-	"location": "Dubai",
-	"price": 200,
+	"location": "Al Barsha",
+	"price": 250,
 	"image": "images/042-note.png",
 	"availableInventory": 5,
-	"spaces": 5,
-	"rating": 5
+	"spaces": 5
 }
 ]
