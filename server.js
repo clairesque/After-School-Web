210247ca92ed40a9a126e1a794a44e2ee91e3652
Clerissa Lewis <clerissalewis@gmail.com>
2021-02-05 10:41:45 +0400

    Added other CRUD requests

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 // created new express app
 const app = express()
-
 app.use(express.json())
 app.set('port', 3000)
 app.use ((req, res, next) => {
@@ -18,7 +17,7 @@ MongoClient.connect('mongodb+srv://MyMongoDBUser:Claire2710@gettingstarted.0k9dq
 })
 
 app.get('/', (req, res, next) => {
-    res.send('Select a collection, e.g., /collection/messages')
+    res.send('Select a collection, e.g., /collection/lessons')
 })
 
 // get the collection name
@@ -33,6 +32,47 @@ app.get('/collection/:collectionName', (req, res, next) => {
         req.collection.find({}, {sort: [['price', -1]]}).toArray((e, results) => {
             if (e) return next(e)
             res.send(results)
+        })
+    })
+
+// post request to add object
+app.post('/collection/:collectionName', (req, res, next) => {  
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e)    
+        res.send(results.ops)  
+    })
+})
+
+// retrieve object by a specific ID
+const ObjectID = require('mongodb').ObjectID;
+app.get('/collection/:collectionName/:id', (req, res, next) => {    
+    req.collection.findOne({ _id: new ObjectID(req.params.id) }, (e, result) => {
+        if (e) return next(e)        
+        res.send(result)    
+    })
+})
+
+// put request to update an object
+app.put('/collection/:collectionName/:id', (req, res, next) => {  
+    req.collection.update(    
+        {_id: new ObjectID(req.params.id)},    
+        {$set: req.body},    
+        {safe: true, multi: false}, (e, result) => {
+            if (e) return next(e)      
+            res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})    
+        })
+    })
+
+// request to delete an object by ID
+app.delete('/collection/:collectionName/:id', (req, res, next) => {    
+    req.collection.deleteOne(        
+        { 
+            _id: ObjectID(req.params.id) 
+        }, 
+        (e, result) => {
+            if (e) return next(e)            
+            res.send((result.result.n === 1) ?                 
+            {msg: 'success'} : {msg: 'error'})    
         })
     })
 
