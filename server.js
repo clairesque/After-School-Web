f8c6b570a9570b0c862de7428151c0e124dfdce5
Clerissa Lewis <clerissalewis@gmail.com>
2021-02-13 20:57:00 +0400

    Changes in server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.use ((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.header("Access-Control-Allow-Headers","*");
     res.setHeader('Access-Control-Allow-Methods', 'PUT');
+    res.setHeader('Access-Control-Allow-Methods', 'PUSH');
     next();
 })
 
